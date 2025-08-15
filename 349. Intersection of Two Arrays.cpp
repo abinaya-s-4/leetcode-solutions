ca18c204902490cb2b7e7000f540453af7ee9d95
Abinaya S <abinaya.selva2020@gmail.com>
2025-08-15 14:47:22 +0530

    349. Intersection of Two Arrays

---
+++ 349. Intersection of Two Arrays.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        unordered_set<int> set1(nums1.begin(),nums1.end());
+        unordered_set<int> result;
+        for( int nums : nums2){
+            if(set1.count(nums)){
+                result.insert(nums);
+            }
+        }
+        return vector<int> (result.begin(),result.end());
+    }
+};
