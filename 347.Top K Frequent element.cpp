038a2c7ceb5255bca5ed79b23df6677f9e5cd293
Abinaya S <abinaya.selva2020@gmail.com>
2025-08-15 22:43:07 +0530

    349. Intersection of Two Arrays

---
+++ 347.Top K Frequent element.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int,int> freq;
+        vector<int> result;
+        for(int num : nums ) freq[num]++;
+        vector<pair<int,int>> element;
+        for(auto &num :freq) element.push_back(num);
+        sort(element.begin(),element.end()),[] (const pair<int,int>&a,const pair<int,int>&b){
+            return a.second>b.second;
+        };
+        for(int i=0;i<k;i++){
+            result.push_back(element[i].first);
+        }
+        return result;
+    }
+};
